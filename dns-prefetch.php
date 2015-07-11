31a37b3bf5629d8da5f07a5013a48ab2130d6d5e
Adem ilter <ademilter@gmail.com>
2015-07-11 23:43:40 +0300

    file edit

--- dns-prefetch.php
+++ dns-prefetch.php
@@ -3,25 +3,27 @@
 <link rel="dns-prefetch" href="//cloud.typography.com"/>
 <!-- google map thumbnail -->
 <link rel="dns-prefetch" href="//maps.google.com">
-<!-- flickr thumbnail -->
+<!-- flickr photo -->
 <link rel="dns-prefetch" href="//farm5.static.flickr.com">
-<!-- facebook thumbnail -->
+<!-- facebook photo -->
 <link rel="dns-prefetch" href="//sphotos.ak.fbcdn.net">
 <!-- dribbble thumbnail -->
 <link rel="dns-prefetch" href="//d13yacurqjgara.cloudfront.net">
-<!-- instagram thumbnail -->
+<!-- gravatar photo -->
+<link rel="dns-prefetch" href="//https://1.gravatar.com">
+<!-- instagram photo -->
 <link rel="dns-prefetch" href="//scontent.cdninstagram.com">
-<!-- lastfm thumbnail -->
+<!-- lastfm song cover photo -->
 <link rel="dns-prefetch" href="//userserve-ak.last.fm">
-<!-- pinterest thumbnail -->
+<!-- pinterest pin photo -->
 <link rel="dns-prefetch" href="//www.eddiesfloralgallery.net">
 <!-- codepen thumbnail -->
 <link rel="dns-prefetch" href="//s3-us-west-2.amazonaws.com">
-<!-- soundcloud thumbnail -->
+<!-- soundcloud song cover photo -->
 <link rel="dns-prefetch" href="//i1.sndcdn.com">
-<!-- etsy thumbnail -->
+<!-- etsy product photo -->
 <link rel="dns-prefetch" href="//img2.etsystatic.com">
-<!-- tumblr thumbnail -->
+<!-- tumblr image -->
 <link rel="dns-prefetch" href="//27.media.tumblr.com">
 <!-- google analytics -->
 <link rel="dns-prefetch" href="//google-analytics.com">
