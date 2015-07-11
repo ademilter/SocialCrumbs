15d582bea16eb74ed31f7ba724b10a5c7cb7993e
Tom J Nowell <contact@tomjn.com>
2015-07-11 23:22:37 +0100

    escape and secure header.php further

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php esc_attr( get_bloginfo('charset') ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title><?php bloginfo('name'); ?></title>
+    <title><?php esc_html( get_bloginfo('name') ); ?></title>
     <meta name="description" content="">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/style.css"/>
 
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="pingback" href="<?php esc_html( get_bloginfo('pingback_url') ); ?>">
     <?php wp_head(); ?>
 
     <?php include_once("analyticstracking.php"); ?>
