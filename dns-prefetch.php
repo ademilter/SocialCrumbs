f3de59f37da298425abdb7798ea37cab1ca3c0ba
Adem ilter <ademilter@gmail.com>
2015-07-11 15:53:47 +0300

    add dns-prefetch

---
+++ dns-prefetch.php
@@ -0,0 +1,28 @@
+<link rel="dns-prefetch" href="//adem.social">
+<!-- typography fonts -->
+<link rel="dns-prefetch" href="//cloud.typography.com"/>
+<!-- google map thumbnail -->
+<link rel="dns-prefetch" href="//maps.google.com">
+<!-- flickr thumbnail -->
+<link rel="dns-prefetch" href="//farm5.static.flickr.com">
+<!-- facebook thumbnail -->
+<link rel="dns-prefetch" href="//sphotos.ak.fbcdn.net">
+<!-- dribbble thumbnail -->
+<link rel="dns-prefetch" href="https://d13yacurqjgara.cloudfront.net">
+<!-- instagram thumbnail -->
+<link rel="dns-prefetch" href="//scontent.cdninstagram.com">
+<link rel="dns-prefetch" href="//distilleryimage7.s3.amazonaws.com">
+<!-- lastfm thumbnail -->
+<link rel="dns-prefetch" href="//userserve-ak.last.fm">
+<!-- pinterest thumbnail -->
+<link rel="dns-prefetch" href="//www.eddiesfloralgallery.net">
+<!-- codepen thumbnail -->
+<link rel="dns-prefetch" href="//s3-us-west-2.amazonaws.com">
+<!-- soundcloud thumbnail -->
+<link rel="dns-prefetch" href="//i1.sndcdn.com">
+<!-- etsy thumbnail -->
+<link rel="dns-prefetch" href="//img2.etsystatic.com">
+<!-- tumblr thumbnail -->
+<link rel="dns-prefetch" href="//27.media.tumblr.com">
+<!-- google analytics -->
+<link rel="dns-prefetch" href="//google-analytics.com">
--- header.php
+++ header.php
@@ -7,10 +7,11 @@
     <title><?php bloginfo('name'); ?></title>
     <meta name="description" content="">
 
-
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <?php include_once("dns-prefetch.php"); ?>
 
     <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7898554/661028/css/fonts.css"/>
     <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/style.css"/>
