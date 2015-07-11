b52d72c48606f26844344519f6fcdd53989be244
Tom J Nowell <contact@tomjn.com>
2015-07-11 23:47:48 +0100

    move most of the header markup into the wp_head hook

--- functions.php
+++ functions.php
@@ -159,3 +159,28 @@ function socialcrumbs_setup_loop( $query ) {
 
 }
 add_action( 'pre_get_posts', 'socialcrumbs_setup_loop' );
+
+
+function socialcrumbs_head() {
+    ?>
+    <meta charset="<?php esc_attr( get_bloginfo('charset') ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title><?php esc_html( get_bloginfo('name') ); ?></title>
+    <meta name="description" content="">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <?php include_once("dns-prefetch.php"); ?>
+
+    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7898554/661028/css/fonts.css"/>
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/style.css"/>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php esc_html( get_bloginfo('pingback_url') ); ?>">
+    <?php
+}
+add_action( 'wp_head', 'socialcrumbs_head' );
+
--- header.php
+++ header.php
@@ -1,28 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php esc_attr( get_bloginfo('charset') ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <head>
+        <?php
+        wp_head();
+        include_once( "analyticstracking.php" );
+        ?>
+    </head>
+    <body <?php body_class(); ?>>
 
-    <title><?php esc_html( get_bloginfo('name') ); ?></title>
-    <meta name="description" content="">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-    <?php include_once("dns-prefetch.php"); ?>
-
-    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7898554/661028/css/fonts.css"/>
-    <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/style.css"/>
-
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php esc_html( get_bloginfo('pingback_url') ); ?>">
-    <?php wp_head(); ?>
-
-    <?php include_once("analyticstracking.php"); ?>
-
-</head>
-<body <?php body_class(); ?>>
-
-<?php include 'icons.php'; ?>
+        <?php include 'icons.php'; ?>
