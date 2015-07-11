75adb84dd284208ea22a1f052cab3ca8ead94cf5
Tom J Nowell <contact@tomjn.com>
2015-07-12 00:28:50 +0100

    move the post code into content.php

---
+++ content.php
@@ -0,0 +1,41 @@
+<?php
+
+$eventType = "";
+$eventTitle = "";
+$eventContent = "";
+$eventUrl = "";
+$eventTimeStamp = sprintf(__('%s ago', 's-report'), human_time_diff(get_post_time('U'), current_time('timestamp')));
+// connect, purchase, link, calendar, like, favorite, bookmark, watch, listen, add, write, checkin
+$eventIconStatus = "";
+$postCategories = get_the_category();
+$eventCategory = '';
+if ( !empty( $postCategories[0] ) ) {
+    $eventCategory = $postCategories[0]->slug;
+}
+$postTags = get_the_tags();
+$tags = array();
+if ( is_array( $postTags ) ) {
+    $tags = array_values($postTags);
+}
+$eventTag = '';
+if ( !empty( $tags ) ) {
+    $eventTag = $tags[0]->slug;
+}
+$content = tokenText( get_the_content() );
+
+// Event Recipes
+include "event/codepen.php";
+include "event/delicious.php";
+include "event/dribbble.php";
+include "event/foursquare.php";
+include "event/github.php";
+include "event/instagram.php";
+include "event/instapaper.php";
+include "event/lastfm.php";
+include "event/soundcloud.php";
+include "event/twitter.php";
+include "event/vimeo.php";
+include "event/youtube.php";
+
+// Event Template
+eventTemplateSidebar($eventType, $eventTitle, $eventContent, $eventUrl, $eventTimeStamp, $eventIconStatus, $eventCategory, $eventTag);
