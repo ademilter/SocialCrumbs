4d8418b2baf9668e6fe40cbb326f33a8bb9a6fcf
Tom J Nowell <contact@tomjn.com>
2015-07-11 23:44:22 +0100

    fix 2 PHP notices

--- functions.php
+++ functions.php
@@ -10,11 +10,13 @@ load_theme_textdomain( 's-report', get_template_directory() . '/languages' );
  * IFTTT Data to Object
  */
 function tokenText( $str ) {
-    $matches = preg_split("/\;\s*\\$/uism", $str);
+    $matches = preg_split( "/\;\s*\\$/uism", $str );
     $object = array();
-    foreach ($matches as $match) {
-        preg_match("/\\$?(\w+)\:(.*)/uism", trim($match), $_matches);
-        $object[$_matches[1]] = rtrim($_matches[2], ";");
+    foreach ( $matches as $match) {
+        preg_match( "/\\$?(\w+)\:(.*)/uism", trim( $match ), $_matches );
+        if ( !empty( $_matches ) ) {
+            $object[ $_matches[1] ] = rtrim( $_matches[2], ";" );
+        }
     }
     return $object;
 }
